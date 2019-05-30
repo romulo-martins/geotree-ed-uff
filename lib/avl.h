1b83b3f393f04ebf0244030fcbc845bbe12e93a9
thaisluca <thaisluca@uol.com.br>
2019-05-29 22:17:53 -0300

    Atualizando lib da AVL

--- lib/avl.h
+++ lib/avl.h
@@ -12,14 +12,7 @@ typedef struct tavl {
 	struct tavl *esq, *dir; // nós da esquerda e da direita
 } TAVL;
 
-TAVL* inicializa_avl(void);
-int maior(int altura_esq, int altura_dir);
-TAVL* rotacao_direita(TAVL *d);
-TAVL* rotacao_esquerda(TAVL *e);
-int altura_arvore_avl(TAVL *a);
-int maior(int altura_esq, int altura_dir);
-int calcula_balanceamento(TAVL *a);
-TAVL* cria_no_avl(int codigo, void* geofig);
+TAVL *insere_no_avl(TAVL *a, int codigo, void *geofig);
 
 
 #endif
