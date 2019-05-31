df724099c6c5f193a8d16c3a179a77bac45595a2
thais.almeida <thais.almeida@radixeng.com.br>
2019-05-31 11:36:52 -0300

    Criando interface contendo as funções para o cálculo das áreas das figuras.

---
+++ lib/areas.c
@@ -0,0 +1,33 @@
+/*
+Arquivo que contém as funções para o cálculo das áreas das figuras.
+*/
+
+#include <stdio.h>
+#include "math.h"
+
+
+//Calcula a área do círculo de acordo com o raio informado pelo usuário.
+float area_circulo(float raio){
+	return M_PI * pow(raio,2);
+}
+
+//Calcula a área do quadrado de acordo com o lado informado pelo usuário.
+float area_quadrado(float lado){
+	return pow(lado, 2);
+}
+
+//Calcula a área do triângulo de acordo com base e altura informados pelo usuário.
+float area_triangulo(float base, float altura){
+	return (base*altura)/2;
+}
+
+//Calcula a área do trapézio de acordo com base menor, base maior e altura informados pelo usuário.
+float area_trapezio(float base_menor, float base_maior, float altura){
+	return ((base_menor + base_maior) * altura)/2;
+}
+
+//Calcula a área do retângulo de acordo com base e altura informados pelo usuário.
+float area_retangulo(float base, float altura){
+	return base*altura;
+}
+
---
+++ lib/areas.h
@@ -0,0 +1,13 @@
+#ifndef AREAS_H
+#define AREAS_H
+
+#include <stdio.h>
+#include "math.h"
+
+float area_circulo(float raio);
+float area_quadrado(float lado);
+float area_triangulo(float base, float altura);
+float area_trapezio(float base_menor, float base_maior, float altura);
+float area_retangulo(float base, float altura);
+
+#endif
