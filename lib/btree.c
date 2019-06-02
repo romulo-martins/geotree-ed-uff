61a74c9d309d7c28d95406e9f761fb84ba25a5ab
VictorOlimpio <victorolimpio@id.uff.br>
2019-06-02 17:45:01 -0300

    Adiciona correçoes a arvore b

--- lib/btree.c
+++ lib/btree.c
@@ -31,38 +31,16 @@ BTree *find(BTree *tree, int key){
     return find(tree->childs[i], key);
 }
 
-/* void print(BTree *tree){
+void print(BTree *tree, int level){
     if(tree){
-        int i;
+        int i, j;
         for(i = 0; i < tree->nkeys; i++){
-            if(!tree->leaf) print(tree->childs[i]);
-            printf("%d \n", tree->keys[i]);
+            print(tree->childs[i], level+1);
+            for(j = 0; j <= level; j++) printf("    ");
+            printf("%d\n", tree->keys[i]);
         }
-        if(!tree->leaf) print(tree->childs[i]);
+        print(tree->childs[i], level+1);
     }
-} */
-
-void print(BTree *tree, int level) {
-    if(tree){
-        int i;
-        for (i = 0; i < level; i++) { printf("  "); }
-
-
-        printf("|");
-        for (i = 0; i < tree->nkeys; i++) {
-            printf("%d|", tree->keys[i]);
-
-        }
-        printf("\n");
-
-
-        for (i = 0; i <= tree->nkeys; i++) {
-            if (!tree->leaf) {
-                print(tree->childs[i], level + 1);
-            }
-        }
-    }
-   
 }
 
 void free_bt(BTree* tree) {
@@ -80,19 +58,19 @@ void free_bt(BTree* tree) {
 BTree *split(BTree *x, BTree *y, int i, int t){
     BTree *z = create(t);
     z->nkeys = t-1;
-    
+    z->leaf = y->leaf;
     for(int j = 0; j < t-1; j++) z->keys[j] = y->keys[j+t]; //copia as chaves de y depois da chave do meio para z
     if(!y->leaf){
         for(int j = 0; j < t; j++){
             z->childs[j] = y->childs[j+t];                  //copia os filhos depois da chave do meio para z
-            free(y->childs[j+t]);
+            y->childs[j+t] = NULL;
         } 
     }
     y->nkeys = t-1;                                                             //atualiza o numero de chaves de y para a primeira metade de y
-    for(int j = x->nkeys+1; j >= (i+1); j--) x->childs[j+1] = x->childs[j];     //como o no tera um novo filho, cria espaço para novo filho
-    x->childs[i+1] = z;                                                 //faz z ser filho do novo nó
-    for(int j = (x->nkeys); j >= i; j--) x->keys[j+1] = x->keys[j];     //move todas as chaves maiores que key
-    x->keys[i] = y->keys[t-1];                                          //copia a chave do meio de y para x
+    for(int j = x->nkeys; j >= i; j--) x->childs[j+1] = x->childs[j];     //como o no tera um novo filho, cria espaço para novo filho
+    x->childs[i] = z;                                                 //faz z ser filho do novo nó
+    for(int j = x->nkeys; j >= i; j--) x->keys[j] = x->keys[j-1];     //move todas as chaves maiores que key
+    x->keys[i-1] = y->keys[t-1];                                          //copia a chave do meio de y para x
     x->nkeys++;
     return x;
 }
@@ -100,36 +78,39 @@ BTree *split(BTree *x, BTree *y, int i, int t){
 BTree *insert_nonfull(BTree *x, int key, int t){
     int i = x->nkeys-1;
     if(x->leaf){
-        while(i >= 0 && key < x->keys[i]){
+        while((i >= 0) && (key < x->keys[i])){
             x->keys[i+1] = x->keys[i];
             i--;
         }
         x->keys[i+1] = key;
-        x->nkeys += 1;
-    }else{
-        while(i >= 0 && key < x->keys[i]) i--;
-        i += 1;
-        if(x->childs[i]->nkeys == (2*t)-1){
-            x = split(x, x->childs[i], i, t);
-            if(key > x->keys[i]) i += 1;
-        }
-        x->childs[i] = insert_nonfull(x->childs[i], key, t);
+        x->nkeys++;
+        return x;
     }
+    while((i >= 0) && (key < x->keys[i])) i--;
+    i++;
+    if(x->childs[i]->nkeys == ((2*t)-1)){
+        x = split(x, x->childs[i], i+1, t);
+        if(key > x->keys[i]) i++;
+    }
+    x->childs[i] = insert_nonfull(x->childs[i], key, t);
     return x;
 }
 
 BTree *insert(BTree *tree, int key, int t){
+    if(find(tree, key)) return tree;
     if(!tree){
         tree = create(t);
         tree->keys[0] = key;
-        tree->nkeys++;
+        tree->nkeys = 1;
         return tree;
     }
     if(tree->nkeys == ((2*t)-1)){
         BTree *new = create(t);
+        new->nkeys = 0;
+        new->leaf = 0;
         new->childs[0] = tree;
-        new = split(new, tree, 0, t);
-        new = insert_nonfull(tree, key, t);
+        new = split(new, tree, 1, t);
+        new = insert_nonfull(new, key, t);
         return new;
     }
     tree = insert_nonfull(tree, key, t);
--- tests/btree_test.c
+++ tests/btree_test.c
@@ -1,7 +1,7 @@
 #include "../lib/btree.h"
 
 int main(int argc, char const *argv[]){
-    BTree *b_tree = new_bt();
+    BTree *b_tree = create(2);
     b_tree = insert(b_tree, 10, 2);
     b_tree = insert(b_tree, 20, 2);
     b_tree = insert(b_tree, 15, 2);
@@ -15,6 +15,16 @@ int main(int argc, char const *argv[]){
 
     print(b_tree, 0);
 
+    if(find(b_tree, 10)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 15)) printf("OK\n"); else printf("ERRO\n");
     if(find(b_tree, 20)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 1)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 5)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 11)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 12)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 16)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 17)) printf("OK\n"); else printf("ERRO\n");
+    if(find(b_tree, 21)) printf("OK\n"); else printf("ERRO\n");
+    if(!find(b_tree, 24)) printf("OK\n"); else printf("ERRO\n");// não deve encotrar
     free_bt(b_tree);
 }
