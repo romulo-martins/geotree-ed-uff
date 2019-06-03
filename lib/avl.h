ac65e6d533bfc930fbd34f64b3c89beb5e071021
ThaisLuca <thaisluca@uol.com.br>
2019-06-03 09:00:52 -0300

    Operações da árvore AVL

--- lib/avl.h
+++ lib/avl.h
@@ -3,16 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <string.h>
 
 // Nó da arvore AVL, possui como informação figuras geométricas
 typedef struct tavl {
 	int codigo; // codigo unico do nó da arvore
 	void* geofig; // a informação do nó, ou seja, a figura geométrica
+	int alt; //altura do nó
 	struct tavl *esq, *dir; // nós da esquerda e da direita
 } TAVL;
 
 TAVL *insere_no_avl(TAVL *a, int codigo, void *geofig);
+TAVL* retira_no_avl(TAVL *a, int x);
+void imprime_arvore_avl(TAVL *a);
+void libera(TAVL *a);
 
 
 #endif
