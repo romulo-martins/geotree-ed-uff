9704668892fffedee2162169c5fe1c97ac1483c0
romulo-martins <romulo_martins@id.uff.br>
2019-06-09 16:15:26 -0300

    Desacoplando a interface da arvore genérica da main.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "lib/gentree.h"
+#include "lib/gentree_interface.h"
 
 void show_welcome(void) {
 	printf("------------------------------------------\n");
@@ -22,122 +23,9 @@ void show_help_opts(void) {
 	printf("Para sair do programa: exit ou quit                 \n");
 }
 
-int is_gt_insert(char* cmd) {
-	return (strcmp(cmd, "insert") == 0) || 
-			(strcmp(cmd, "-in") == 0);
-}
-
-GenTree* get_gt_insert(GenTree* t) {
-	int cod, cod_par;
-	char geo_fig[50];
-	scanf("%d/%d/%s", &cod, &cod_par, geo_fig);
-	TFIGURA *f = get_geofig(geo_fig);
-	if(!f) {
-		printf("Error: Figura geométrica inválida.\n");
-	} else {
-		t = insert_gt(t, cod, cod_par, f);
-	}
-	return t;
-}
-
-int is_gt_remove(char* cmd) {
-	return (strcmp(cmd, "remove") == 0)	||
-			(strcmp(cmd, "-rm") == 0);
-}
-
-GenTree* get_gt_remove(GenTree* t) {
-	int cod;
-	scanf("%d", &cod);
-	t = remove_gt(t, cod);
-	return t;
-}
-
-int is_gt_search(char* cmd) {
-	return (strcmp(cmd, "search") == 0) ||
-			(strcmp(cmd, "-s") == 0);
-}
-
-GenTree* get_gt_search(GenTree *t) {
-	printf("TODO: Busca\n"); // TODO: fazer a busca
-	return t;
-}
-
-GenTree* get_gt_edit(GenTree *t){
-	int cod;
-	scanf("%d", &cod);
-	return edit_gt(t, cod);
-}
-
-int is_gt_print(char* cmd) {
-	return (strcmp(cmd, "print") == 0) || 
-			(strcmp(cmd, "-p") == 0);
-}
-
-GenTree* get_gt_print(GenTree *t) {
-	print_gt(t); 
-	return t;
-}
-
-int is_gt_destroy(char* cmd) {
-	return (strcmp(cmd, "destroy") == 0) ||
-			(strcmp(cmd, "-d") == 0);
-}
-
-GenTree* get_gt_destroy(GenTree* t) {
-	free_gt(t);
-	return NULL;
-}
-
-int is_gt_convert(char* cmd) {
-	return (strcmp(cmd, "convert") == 0) ||
-			(strcmp(cmd, "-c") == 0);
-}
-
-int is_gt_edit(char* cmd){
-	return (strcmp(cmd, "edit") == 0) || (strcmp(cmd, "-e") == 0);
-}
-
-GenTree* get_gt_convert(GenTree *t) {
-	printf("TODO: Conversão\n"); // TODO: fazer a conversão
-	return t;
-}
-
-int is_gt_load(char* cmd) {
-	return (strcmp(cmd, "load") == 0) ||
-			(strcmp(cmd, "-l") == 0);
-}
-
-GenTree* get_gt_load(GenTree* t) {
-	char path[100];
-	scanf("%s", path);
-	t = load_gt(t, path);
-	return t;
-}
-
-GenTree* gentree_cmds(GenTree* t) {
-	char cmd[50];
-	scanf("%s", cmd);
-
-	if(is_gt_insert(cmd))  return get_gt_insert(t);
-	if(is_gt_remove(cmd))  return get_gt_remove(t);
-	if(is_gt_search(cmd))  return get_gt_search(t);
-	if(is_gt_print(cmd))   return get_gt_print(t);
-	if(is_gt_destroy(cmd)) return get_gt_destroy(t);
-	if(is_gt_convert(cmd)) return get_gt_convert(t);
-	if(is_gt_load(cmd))    return get_gt_load(t);
-	if(is_gt_edit(cmd))	   return get_gt_edit(t);
-	printf("Error: operação não encontrada para GenTree!\n");
-	return t;
-}
-
 int is_help_cmd(char* cmd) {
 	return (strcmp(cmd, "--help") == 0) ||
 			(strcmp(cmd, "-h") == 0); 
-}
-
-int is_tree_cmd(char* cmd) {
-	return (strcmp(cmd, "gentree") == 0) || 
-			(strcmp(cmd, "gt") == 0);
 }
 
 int is_exit(char* cmd) {
@@ -146,7 +34,7 @@ int is_exit(char* cmd) {
 }
 
 int is_a_cmd(char* cmd) {
-	return is_tree_cmd(cmd) || is_help_cmd(cmd);
+	return is_gt_cmd(cmd) || is_help_cmd(cmd);
 }	
 
 int main(int argc, char const *argv[]) {
@@ -159,7 +47,7 @@ int main(int argc, char const *argv[]) {
 		if(is_exit(comand)) break;
 		if(is_a_cmd(comand)) {
 			if(is_help_cmd(comand)) show_help_opts();
-			if(is_tree_cmd(comand)) t = gentree_cmds(t);
+			if(is_gt_cmd(comand)) t = gentree_cmds(t);
 		} else {
 			printf("Error: comando inválido!\n");
 		}
